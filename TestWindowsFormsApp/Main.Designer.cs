0a2869cb7cf1ef7a8fc63b6485eac9fec96bd163
桑榆肖物 <sang93@qq.com>
2024-06-28 23:49:04 +0800

    feat: 添加net481测试

---
+++ TestWindowsFormsApp/Main.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TestWindowsFormsApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTr = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTr
+            // 
+            this.btnTr.Location = new System.Drawing.Point(158, 204);
+            this.btnTr.Name = "btnTr";
+            this.btnTr.Size = new System.Drawing.Size(75, 23);
+            this.btnTr.TabIndex = 0;
+            this.btnTr.Text = "Translate";
+            this.btnTr.UseVisualStyleBackColor = true;
+            this.btnTr.Click += new System.EventHandler(this.btnTr_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 63);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(378, 120);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "Hello, Sang Baidu Translate API Library";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 27);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 21);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(149, 27);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 21);
+            this.txtKey.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "appid";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "key";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 249);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnTr);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTr;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestWindowsFormsApp/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sang.Baidu.TranslateAPI;
+
+namespace TestWindowsFormsApp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTr_Click(object sender, EventArgs e)
+        {
+            string appid = txtId.Text;
+            string appkey = txtKey.Text;
+            string text = textBox.Text;
+
+            if (string.IsNullOrEmpty(appid) || string.IsNullOrEmpty(appkey) || string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            var translate = new BaiduTranslator(appid, appkey);
+            translate.Translate(text,"zh").ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    MessageBox.Show(t.Exception.Message);
+                    return;
+                }
+
+                BaiduTranslateResult result = t.Result;
+                if (result.Success)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        textBox.Text = result.Trans_Result[0].Dst;
+                    }));
+                }
+            });
+            
+        }
+    }
+}
